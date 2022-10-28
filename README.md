85ec9ba509a6d567522e022326ea4303d923d1df
7H3-PR073C70R <okanlawondamilare24434@gmail.com>
2022-10-28 15:01:14 +0300

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# HNG Backend Task 1
+
+A simple server that returns my info.
+
+ExpressJS is the way 🚀🚀🚀
+
+## Getting started
+clone repo
+```$
+git clone https://github.com/7H3-PR073C70R/hng_backend_task_1
+```
+
+## start server
+run
+```$
+npm install
+```
+then 
+```$
+npm run dev
+```
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+
+
+app.get('/', function (req, res) {
+    res.status(200).send( { "slackUsername": "toxic-bishop", "backend": true, "age": 24, "bio": "Aspiring full stack mobile dev but for now I am a highly motivated Mobile Application Developer with extensive knowledge and hands-on experience working on Smartphone Applications using flutter." });
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
