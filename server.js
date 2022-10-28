ed53dd5f3f7ab5a4cd54e17d61be74aff7184d36
7H3-PR073C70R <okanlawondamilare24434@gmail.com>
2022-10-28 15:45:41 +0300

    cors

--- server.js
+++ server.js
@@ -3,8 +3,22 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 
-app.get('/', function (req, res) {
-    res.status(200).send( { "slackUsername": "toxic-bishop", "backend": true, "age": 27, "bio": "Aspiring full stack mobile dev but for now I am a highly motivated Mobile Application Developer with extensive knowledge and hands-on experience working on Smartphone Applications using flutter." });
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "GET"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  }).get('/', function (req, res) {
+    res.status(200).send( { "slackUsername": "toxic-bishop", "backend": true, "age": 24, "bio": "Aspiring full stack mobile dev but for now I am a highly motivated Mobile Application Developer with extensive knowledge and hands-on experience working on Smartphone Applications using flutter." });
 });
 
 app.listen(port, () => {
